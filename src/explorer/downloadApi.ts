3007975edf4f3b5b50bd8a4f4d0f3d359416afb6
DESKTOP-O2MV8V9\user <zhiyuan.li@rjoy.com>
2023-04-20 19:30:27 +0800

    优化下载

--- src/explorer/downloadApi.ts
+++ src/explorer/downloadApi.ts
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio';
 import { readerDriver } from '../reader';
 import { TreeNode } from './TreeNode';
 import { writeFile } from 'fs';
-
+import workspaceConfiguration from '../utils/workspaceConfiguration';
 
 export default class DownLoadApi {
     
@@ -39,16 +39,30 @@ export default class DownLoadApi {
         _opcMsg.unshift(`${_index + 1} / ${_maxCp}`);
         this.showOutMsg([..._opcMsg].reverse().join("\n"));
 
-        while (_index < _maxCp) {
-            let _node = chapters[_index];
-            // let _title = _node.name;
-            _bookData.push(_node.name);
-            let _art = await this.getContext(_node);
-            _bookData.push(_art);
-            _index++;
+        // while (_index < _maxCp) {
+        //     let _node = chapters[_index];
+        //     // let _title = _node.name;
+        //     _bookData.push(_node.name);
+        //     let _art = await this.getContext(_node);
+        //     _bookData.push(_art);
+        //     _index++;
 
-            _opcMsg[0] = `${_index + 1} / ${_maxCp}`;
+        //     _opcMsg[0] = `${_index + 1} / ${_maxCp}`;
+        //     this.showOutMsg([..._opcMsg].reverse().join("\n"));
+        // }
+        const _tool = new DownLoadTool();
+        _tool.addChangeListener((cur: number, max: number) => {
+            _opcMsg[0] = `${cur} / ${max}`;
             this.showOutMsg([..._opcMsg].reverse().join("\n"));
+        }, this);
+        _tool.init(chapters);
+
+        let _rsl = await _tool.downLoad();
+
+        for (let i = 0; i < _rsl.length; i++) {
+            const element = _rsl[i];
+            _bookData.push(element.title);
+            _bookData.push(element.context);
         }
 
         const _endTS = new Date().getTime();
@@ -86,6 +100,7 @@ export default class DownLoadApi {
         // console.log($("#app > .content").text());
     }
 
+
     private static getContext(chapterNode: TreeNode): Promise<string> {
         return new Promise((resolve: (v: string)=>void) => {
             readerDriver.getContent(chapterNode)
@@ -108,4 +123,106 @@ export default class DownLoadApi {
         this._opc.clear();
         this._opc.appendLine(msg);
     }
+}
+
+
+class DownLoadTool {
+    constructor() {
+        this._chapters = [];
+        this._maxLine = workspaceConfiguration().get("downloadThreads", 10);
+        this._inloading = 0;
+        this._loadIndex = 0;
+        this._completeNum = 0;
+        this._datas = [];
+        this._onChange = undefined;
+        this._callObj = undefined;
+    }
+
+    private _maxLine: number;
+    private _chapters: TreeNode[];
+    private _inloading: number;
+    private _datas: {title: string, context: string}[];
+    private _onChange: ((cur: number, max: number)=>void) | undefined;
+    private _callObj: any;
+
+    private _loadCompleteCall: (()=>void) | undefined;
+    private _loadIndex: number;
+    private _completeNum: number;
+
+    public addChangeListener(call: (cur: number, max: number)=>void, callObj: any) {
+        this._onChange = call;
+        this._callObj = callObj;
+    }
+
+    public init(chapters: TreeNode[]) {
+        this._chapters = [...chapters];
+        this._inloading = 0;
+        this._loadIndex = 0;
+        this._datas = [];
+        this._completeNum = 0;
+    }
+
+    public async downLoad(): Promise<Array<{title: string, context: string}>> {
+        return new Promise((resolve: (data: {title: string, context: string}[])=>void) => {
+            this._loadCompleteCall = () => {
+                resolve(this._datas);
+            }
+
+            this.checkLoadStart();
+        })
+    }
+
+    private checkLoadStart() {
+        for (let i = 0; i < this._maxLine; i++) {
+            this.getContext(i);
+        }
+    }
+
+    private loadEnd(index: number, context: string) {
+
+        this._datas[index] = {
+            title: this._chapters[index].name,
+            context: context
+        }
+
+        this._completeNum++;
+
+        if (this._onChange && this._callObj) {
+            this._onChange.apply(this._callObj, [this._completeNum, this._chapters.length]);
+        }
+
+        this._inloading--;
+
+        if (this._loadIndex == this._chapters.length) {
+            if (this._inloading == 0) {
+                this._loadCompleteCall && this._loadCompleteCall();
+            }
+            
+            return;
+        }
+
+        this.getContext(this._loadIndex);
+    }
+    
+    private getContext(index: number) {
+        this._inloading++;
+        this._loadIndex++;
+
+        if (index == this._chapters.length) {
+            this.loadEnd(index, "章节加载失败");
+        }
+
+        readerDriver.getContent(this._chapters[index])
+        .then((_html: string) => {
+            const $ = cheerio.load(_html);
+            // resolve($("#app > .content").text());
+            const _ctx = $("#app > .content").text();
+            this.loadEnd(index, _ctx);
+        })
+        .catch(() => {
+            // resolve("章节加载失败")
+            this.loadEnd(index, "章节加载失败");
+        })
+    }
+
 }
